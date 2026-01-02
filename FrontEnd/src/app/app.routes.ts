d558d3980cb99875052c6d3e6951edfdd408dee6
Negm <ngmnot44@gmail.com>
2026-01-02 19:09:08 +0200

    Initial commit: backend and frontend setup

---
+++ FrontEnd/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+
+
+
+export const routes: Routes = [
+    {path:'home' , component:Home},
+    {path:'' , redirectTo:'home' , pathMatch:'full'}
+
+
+];
