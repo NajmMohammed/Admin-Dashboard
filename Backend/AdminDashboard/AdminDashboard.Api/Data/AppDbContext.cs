0bd006684ea98e48d0e9d37b2c37806b403e52b2
Negm <ngmnot44@gmail.com>
2026-01-03 17:03:42 +0200

    Configure EF Core with in-memory database

---
+++ Backend/AdminDashboard/AdminDashboard.Api/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AdminDashboard.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminDashboard.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
