bcaa8fd7666cab828f2da8bf3dabc055cb915408
Negm <ngmnot44@gmail.com>
2026-01-03 14:59:21 +0200

    Add UsersController with test endpoint

---
+++ Backend/AdminDashboard/AdminDashboard.Api/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using AdminDashboard.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminDashboard.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new List<User>
+            {
+                new User { Id = 1 , Email = "Admin@test.com" , Role = Role.admin },
+                new User { Id = 2 , Email = "user@test.com" , Role = Role.user }
+            };
+            return Ok(users);
+
+        }
+    }
+}
