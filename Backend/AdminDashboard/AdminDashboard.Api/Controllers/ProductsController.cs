49e32ebcd4cfcd9d012a5f172c7ba4d87d94353a
Negm <ngmnot44@gmail.com>
2026-01-03 15:06:04 +0200

    Add Product model and ProductsController

---
+++ Backend/AdminDashboard/AdminDashboard.Api/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using AdminDashboard.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminDashboard.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = new List<Product>
+            {
+                new Models.Product { Id = 1 , Name = "Product 1" , Description = "Description for Product 1" , Price = 10.99m , Stock = 100 },
+                new Models.Product { Id = 2 , Name = "Product 2" , Description = "Description for Product 2" , Price = 20.99m , Stock = 50 }
+            };
+            return Ok(products);
+        }
+    }
+}
