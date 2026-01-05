15beeb582b661c8b519fba4e218b2cdef3f734b3
Negm <ngmnot44@gmail.com>
2026-01-05 21:20:13 +0200

    Add DTOS

---
+++ Backend/AdminDashboard/AdminDashboard.Api/DTOS/ProductDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminDashboard.Api.DTOS
+{
+    public class ProductDTO
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ Backend/AdminDashboard/AdminDashboard.Api/DTOS/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminDashboard.Api.DTOS
+{
+    public class UserDTO
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+    }
+}
