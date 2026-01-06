e30ecaa46a0fca7b83a5f739a3b2c6c933e5a188
Negm <ngmnot44@gmail.com>
2026-01-06 19:52:09 +0200

    Added Update DTOS

---
+++ Backend/AdminDashboard/AdminDashboard.Api/DTOS/UpdateProductDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminDashboard.Api.DTOS
+{
+    public class UpdateProductDTO
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
