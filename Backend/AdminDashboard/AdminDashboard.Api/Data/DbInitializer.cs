9208d98ec79ac8f6903c4d69a77fba636ffc651a
Negm <ngmnot44@gmail.com>
2026-02-10 18:13:03 +0200

    Added Automatic Admin User Seeding At startup

---
+++ Backend/AdminDashboard/AdminDashboard.Api/Data/DbInitializer.cs
@@ -0,0 +1,25 @@
+﻿using AdminDashboard.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminDashboard.Api.Data
+{
+    public static class DbInitializer
+    {
+        public static async Task SeedAdminAsync (AppDbContext context)
+        {
+            if (await context.Users.AnyAsync(u => u.Role == "admin"))
+                return;
+            var passwordHasher = new PasswordHasher<User>();
+            var admin = new User
+            {
+                Name = "Admin",
+                Email = "admin@dashboard.com",
+                Role = "admin"
+            };
+            admin.Password = passwordHasher.HashPassword(admin, "Admin123");
+            context.Users.Add(admin);
+            await context.SaveChangesAsync();
+        }
+    }
+}
